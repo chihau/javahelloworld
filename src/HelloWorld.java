e76ff1f6e55a16596f6b9ff15b267e2ce3fd6298
Chihau Chau <chihau@gmail.com>
2017-06-28 13:08:30 +0000

    seventh commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 		System.out.println("Hello World again 3");
 		System.out.println("Hello World again 4");
 		System.out.println("Hello World again 5");
+		System.out.println("Hello World again 6");
 	}
 }
